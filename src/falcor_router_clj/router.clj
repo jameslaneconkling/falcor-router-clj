ee4fbfa43c8773044b82188a6be4b418689f0534
James Conkling <james.lane.conkling@gmail.com>
2018-03-28 14:25:58 -0400

    always return git log

--- src/falcor_router_clj/router.clj
+++ src/falcor_router_clj/router.clj
@@ -21,87 +21,46 @@
     (group-by pattern-key key-setv)))
 
 
-(defn match-path-set*
-  [pattern path-set]
-  (reduce (fn [parsed [pattern-key key-set]]
-            (cond
-              ))
-          {:unmatched [] :matched []}
-          (map vector pattern path-set)))
-
-
 ;; TODO - this could be rewritten as a reduction over the pattern
 (defn match-path-set
   ([pattern path-set]
    (if (< (count path-set) (count pattern))
      ;; failed pattern match: path-set too short
-     {:unmatched [path-set] :matched []}
+     {:unmatched [path-set] :matched [] :remaining []}
      (loop [[pattern-key & rest-pattern] pattern
-            [key-set & rest-path-set :as path-set] path-set
+            [key-set & rest-path-set] path-set
             previous-path-set []
             parsed {:unmatched [] :matched []}]
-       (cond
-         ;; successful pattern match
-         (and (nil? pattern-key) (empty? rest-path-set)) parsed
-         ;; successful pattern match [with remaining path-sets]
-         (nil? pattern-key) (assoc parsed :remaining path-set)
-         :else (let [{matched true unmatched false} (test-pattern pattern-key key-set)]
-                 (if (empty? matched)
-                   ;; failed pattern match: key-set doesn't match pattern
-                   {:unmatched [(concat previous-path-set path-set)] :matched []}
-                   ;; successful key-set match
-                   (recur rest-pattern
-                          rest-path-set
-                          (conj previous-path-set key-set)
-                          (cond-> parsed
-                            (not (empty? unmatched)) (update :unmatched conj (concat previous-path-set
-                                                                                     unmatched
-                                                                                     rest-path-set))
-                            true (update :matched conj matched))))))))))
+       (let [{matched true unmatched false} (test-pattern pattern-key key-set)]
+         (cond
+           ;; failed pattern match: key-set doesn't match pattern
+           (empty? matched) {:unmatched [path-set] :matched [] :remaining []}
+           ;; successful pattern match
+           (empty? rest-pattern) (cond-> parsed
+                                   (not (empty? unmatched)) (update :unmatched
+                                                                    conj
+                                                                    (concat previous-path-set
+                                                                            unmatched
+                                                                            rest-path-set))
+                                   true (update :matched conj matched)
+                                   true (assoc :remaining (or rest-path-set [])))
+           ;; successful key-set match
+           :else (recur rest-pattern
+                        rest-path-set
+                        (conj previous-path-set key-set)
+                        (cond-> parsed
+                          (not (empty? unmatched)) (update :unmatched
+                                                           conj
+                                                           (concat previous-path-set
+                                                                   unmatched
+                                                                   rest-path-set))
+                          true (update :matched conj matched)))))))))
 
 
-(defn is-equal
-  [actual expected]
-  (if-not (= actual expected)
-    (throw (AssertionError. (str "Expected:\t" expected "\n\nActual:\t" actual)))))
-
-
-(let [pattern [(partial = "resource") key? key? range?]]
-
-  ;; test match
-  (is-equal (match-path-set pattern
-                            ["resource" ["one" "two"] "label" 0])
-            {:unmatched []
-             :matched [["resource"] ["one" "two"] ["label"] [0]]})
-
-  ;; test unmatched - bad pattern
-  (is-equal (match-path-set pattern
-                            ["resource" {:to 10} "label" ["abc" {:to 1}]])
-            {:unmatched [["resource" {:to 10} "label" ["abc" {:to 1}]]]
-             :matched []})
-
-  ;; test unmatched - short pathset
-  (is-equal (match-path-set pattern
-                            ["resource" ["one" "two"] "label"])
-            {:unmatched [["resource" ["one" "two"] "label"]]
-             :matched []})
-
-  ;; test match and unmatched
-  ;; TODO - BUG: intersecting path bt/ two unmatched path-sets: ["resource" {:to 10} "label" "abc"] appears in both
-  ;; this path would be dispatched twice against future matching paths
-  (is-equal (match-path-set pattern
-                            ["resource" ["one" {:to 10}] "label" ["abc" {:to 1}]])
-            {:unmatched [["resource" {:to 10} "label" ["abc" {:to 1}]]
-                         ["resource" ["one" {:to 10}] "label" "abc"]]
-             :matched [["resource"] ["one"] ["label"] [{:to 1}]]})
-
-  ;; test remaining
-  (is-equal (match-path-set pattern
-                            ["resource" "one" "relation" 0 "label" 0])
-            {:unmatched []
-             :matched [["resource"] ["one"] ["relation"] [0]]
-             :remaining ["label" 0]}))
-
+;; (defn is-equal
+;;   [actual expected]
+;;   (if-not (= actual expected)
+;;     (throw (AssertionError. (str "Expected:\t" expected "\n\nActual:\t" actual)))))
 
 (defn merge-parsed
   [parsed-path {:keys [matched unmatched remaining]}]
--- test/falcor_router_clj/router_test.clj
+++ test/falcor_router_clj/router_test.clj
@@ -18,19 +18,22 @@
       (is-equal (match-path-set pattern
                                 ["resource" ["one" "two"] "label" 0])
                 {:unmatched []
-                 :matched [["resource"] ["one" "two"] ["label"] [0]]})
+                 :matched [["resource"] ["one" "two"] ["label"] [0]]
+                 :remaining []})
 
-      ;; test unmatched
+      ;; test unmatched - bad pattern
       (is-equal (match-path-set pattern
                                 ["resource" {:to 10} "label" ["abc" {:to 1}]])
                 {:unmatched [["resource" {:to 10} "label" ["abc" {:to 1}]]]
-                 :matched []})
+                 :matched []
+                 :remaining []})
 
       ;; test unmatched - short pathset
       (is-equal (match-path-set pattern
                                 ["resource" ["one" "two"] "label"])
                 {:unmatched [["resource" ["one" "two"] "label"]]
-                 :matched []})
+                 :matched []
+                 :remaining []})
 
       ;; test match and unmatched
       ;; TODO - BUG: intersecting path bt/ two unmatched path-sets: ["resource" {:to 10} "label" "abc"] appears in both
@@ -39,7 +42,8 @@
                                 ["resource" ["one" {:to 10}] "label" ["abc" {:to 1}]])
                 {:unmatched [["resource" {:to 10} "label" ["abc" {:to 1}]]
                              ["resource" ["one" {:to 10}] "label" "abc"]]
-                 :matched [["resource"] ["one"] ["label"] [{:to 1}]]})
+                 :matched [["resource"] ["one"] ["label"] [{:to 1}]]
+                 :remaining []})
 
       ;; test remaining
       (is-equal (match-path-set pattern
@@ -48,25 +52,26 @@
                  :matched [["resource"] ["one"] ["relation"] [0]]
                  :remaining ["label" 0]})))
 
-  (t/testing "match path sets"
-    (let [pattern [(partial = "resource") key? key? range?]]
 
-      ;; test matched
-      (is-equal (match-path-sets pattern
-                                 [["resource" ["one" "two"] "label" 0]
-                                  ["resource" "three" "relation" {:to 4}]])
-                {:unmatched []
-                 :matched [{:paths [["resource"] ["three"] ["relation"] [{:to 4}]]
-                            :remaining nil}
-                           {:paths [["resource"] ["one" "two"] ["label"] [0]]
-                            :remaining nil}]})
+    (t/testing "match path sets"
+      (let [pattern [(partial = "resource") key? key? range?]]
 
-      ;; test unmatched
-      (is-equal (match-path-sets pattern
-                                 [["resource" {:to 10} "label" [0 {:to 1}]]
-                                  ["resource" ["one" {:to 10}] "relation" ["abc" {:to 1}]]])
-                {:unmatched [["resource" {:to 10} "label" [0 {:to 1}]]
-                             ["resource" {:to 10} "relation" ["abc" {:to 1}]]
-                             ["resource" ["one" {:to 10}] "relation" "abc"]]
-                 :matched [{:paths [["resource"] ["one"] ["relation"] [{:to 1}]]
-                            :remaining nil}]}))))
+        ;; test matched
+        (is-equal (match-path-sets pattern
+                                   [["resource" ["one" "two"] "label" 0]
+                                    ["resource" "three" "relation" {:to 4}]])
+                  {:unmatched []
+                   :matched [{:paths [["resource"] ["three"] ["relation"] [{:to 4}]]
+                              :remaining []}
+                             {:paths [["resource"] ["one" "two"] ["label"] [0]]
+                              :remaining []}]})
+
+        ;; test unmatched
+        (is-equal (match-path-sets pattern
+                                   [["resource" {:to 10} "label" [0 {:to 1}]]
+                                    ["resource" ["one" {:to 10}] "relation" ["abc" {:to 1}]]])
+                  {:unmatched [["resource" {:to 10} "label" [0 {:to 1}]]
+                               ["resource" {:to 10} "relation" ["abc" {:to 1}]]
+                               ["resource" ["one" {:to 10}] "relation" "abc"]]
+                   :matched [{:paths [["resource"] ["one"] ["relation"] [{:to 1}]]
+                              :remaining []}]}))))
